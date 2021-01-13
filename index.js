acd47840c63da8ad40b09815028773aa76a95031
kimlimjustin <kimlimjustin@gmail.com>
2021-01-13 14:33:03 +0700

    Updated tokenizer and update index.js file

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
+const {Tokenize} = require('./src/tokenizer');
+const {Lex} = require('./src/lexer');
 // Code for command line command
 const cli = () => {
   const args = process.argv.slice(2)
     if(args.length > 0){
-      let {Tokenize} = require('./src/tokenizer');
       let file = args[0];
       // Check file extension
-      if(!(file.endsWith('.am') || file.endsWith('.abstractmark'))) throw new Error('AbstractMark: only file with extension .am or .abstractmark is allowed')
+      if(!(file.endsWith('.am') || file.endsWith('.abstractmark'))) throw new Error('\x1b[31mAbstractMark: only file with extension .am or .abstractmark is allowed\x1b[0m')
       // Read file data
       const fs = require('fs');
       let data = fs.readFileSync(file, 'utf-8')
-      console.log(Tokenize(data))
+      const tokenizedData = Tokenize(data)
+      console.log(tokenizedData)
+      Lex(tokenizedData)
     }
 }
 
 const AbstractMark = (abstractmark) => {
-  let {Tokenize} = require('./src/tokenizer');
   let TokenizedData = Tokenize(abstractmark)
   return TokenizedData
 }
--- src/tokenizer.js
+++ src/tokenizer.js
@@ -1,6 +1,8 @@
 const Tokenize = (abstractmark) => {
     let tokenizedData = [];
     let data = abstractmark.split('\n');
+    //List to determine ammount of tab regardless the indentation
+    let totalSpaceArray = []
     for (let i = 0; i < data.length; i++){
         let newData = {}
         //Checking wether the line is the last line
@@ -13,16 +15,34 @@ const Tokenize = (abstractmark) => {
         if(data[i].charCodeAt(0) === 32 || data[i].charCodeAt(0) === 9) newData.hasTab = true
         else newData.hasTab = false
         //Count the ammount of spaces or tabs
-        var totalTab = 0
-        for(let j = 0; j< data[i].length; i++){
-            if(data[i].charCodeAt(j) !== 32 && data[i].charCodeAt(j) !== 9) break
+        var totalSpace = 0
+        var totalTabs = 0
+        for(let j = 0; j< data[i].length; j++){
+            if(data[i].charCodeAt(j) !== 32 && data[i].charCodeAt(j) !== 9) j = data[i].length // Break only the inner loop
             if(data[i].charCodeAt(j) === 9) totalTab += 1
-            else if (data[i].charCodeAt(j) === 32) totalTab += 1
+            else if (data[i].charCodeAt(j) === 32) totalSpace += 1
         }
-        newData.totalTab = totalTab
+        newData.totalSpace = totalSpace
+        newData.totalTabs = totalTabs
+        totalSpaceArray.push(totalSpace)
         //Remove indentation from the value
         newData.value = newData.value.trim()
         tokenizedData.push(newData)
+    }
+    //Greatest common factor function used for finding indentation space.
+    const GCF = (a, b) => {
+        if(b === 0) return a;
+        else return GCF(b, a % b);
+    }
+    const findIndentationSpace = arrayOfTotalSpace => arrayOfTotalSpace.reduce(GCF)
+    const indentationSpace = findIndentationSpace(totalSpaceArray)
+    //Find the total tabs of each line and delete the totalspace
+    for(let i = 0; i< tokenizedData.length; i++){
+        // Only continue if totalSpace is not null and is not undefined
+        if(tokenizedData[i].totalSpace !== null && tokenizedData[i].totalSpace !== undefined){
+            tokenizedData[i].totalTabs = tokenizedData[i].totalSpace / indentationSpace
+            delete tokenizedData[i].totalSpace
+        }
     }
     return tokenizedData
 }
