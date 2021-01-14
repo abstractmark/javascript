143fbb74cc6907498f014ea63e4a265e9a8d9d8c
stanleyowen <69080584+stanleyowen@users.noreply.github.com>
2021-01-14 21:01:00 +0700

    Added Unordered and Ordered List in Lexer

--- src/lexer.js
+++ src/lexer.js
@@ -4,6 +4,18 @@ const Lex = (tokenizedData) => {
         let newData = {};
         // Showing what each line includes.
         newData.includes = {};
+        // Check whether the line is Thematic Break
+        if(tokenizedData[i].value.indexOf("---") === 0) newData.includes.horizontalRule = true
+        else newData.includes.horizontalRule = false
+        // Check whether the line is Ordered List
+        if(!isNaN(tokenizedData[i].value.split(".")[0])){
+            if(tokenizedData[i].value.split(".")[1] && tokenizedData[i].value.split(".")[1].indexOf(' ') === 0){
+                newData.includes.orderedList = true
+            }else newData.includes.orderedList = false
+        }else newData.includes.orderedList = false
+        // Check whether the line is Unordered List
+        if(tokenizedData[i].value.indexOf("-") === 0 && tokenizedData[i].value.charCodeAt(0) === 45 && tokenizedData[i].value.indexOf(' ') === 1) newData.includes.unorderedList = true
+        else newData.includes.unorderedList = false
         // Check whether the line is heading
         if(tokenizedData[i].value.indexOf("#") === 0 && tokenizedData[i].value.charCodeAt(1) === 32) newData.includes.heading = true
         else newData.includes.heading = false
