bb79fb47527a33b7209dd1d301ca00b949e52b94
kimlimjustin <kimlimjustin@gmail.com>
2021-01-16 20:45:22 +0700

    Lexer done

--- src/lexer.js
+++ src/lexer.js
@@ -4,6 +4,24 @@ const Lex = (tokenizedData) => {
         let newData = {};
         // Showing what each line includes.
         newData.includes = {};
+        // Check whether the line contains inline style
+        if(/(.*?)\{((?!(\#|\!)))(.*?)\}/.test(tokenizedData[i].value)) newData.includes.inlineStyle = true
+        else newData.includes.inlineStyle = false
+        // Check whether the line contains class usage
+        if(/(.*?)\{\!(.*?)\}/.test(tokenizedData[i].value)) newData.includes.classUsage = true
+        else newData.includes.classUsage = false
+        // Check whether the line contains heading ID
+        if(/(.*?)\{\#(.*?)\}/.test(tokenizedData[i].value)) newData.includes.headingId = true
+        else newData.includes.headingId = false
+        // Check whether the line contains image
+        if(/!\[[^\]]*\]\((.*?)\s*("(?:.*[^"])")?\s*\)/.test(tokenizedData[i].value)) newData.includes.image = true
+        else newData.includes.image = false
+        // Check whether the line contains link
+        if(/^((?!\!))\[.+\]\(.+\)/.test(tokenizedData[i].value)) newData.includes.link = true
+        else newData.includes.link = false
+        // Check whether the line contains table
+        if(/\|(.*?)\|/.test(tokenizedData[i].value)) newData.includes.table = true
+        else newData.includes.table = false
         // Check whether the line contains bold text
         if(/\*\*(.*?)\*\*/.test(tokenizedData[i].value)) newData.includes.bold = true
         else newData.includes.bold = false
@@ -19,9 +37,18 @@ const Lex = (tokenizedData) => {
         // Check whether the line contains escape character
         if(/\\[^A-Z0-9a-z]/.test(tokenizedData[i].value)) newData.includes.escapeChar = true
         else newData.includes.escapeChar = false
+        // Check whether the line is class definition
+        if(tokenizedData[i].value === "---define") newData.includes.defineClass = true
+        else newData.includes.defineClass = false
         // Check whether the line is Thematic Break
         if(tokenizedData[i].value === "---") newData.includes.horizontalRule = true
         else newData.includes.horizontalRule = false
+        // Check whether the line is a fenced code block
+        if(tokenizedData[i].value === "```") newData.includes.fencedCodeBlock = true
+        else newData.includes.fencedCodeBlock = false
+        // Check wheter the line is a inline code
+        if(/\`(.*?)\`/.test(tokenizedData[i].value)) newData.includes.inlineCode = true
+        else newData.includes.inlineCode = false
         // Check whether the line is Ordered List
         if(/^\d+\. ./.test(tokenizedData[i].value)) newData.includes.orderedList = true
         else newData.includes.orderedList = false
