f7ec269d3e2676be90c0dcc69c88e0b716718574
kimlimjustin <kimlimjustin@gmail.com>
2021-01-18 15:37:30 +0700

    Added heading and pragraph parser

--- src/parser.js
+++ src/parser.js
@@ -3,11 +3,27 @@ const Parse = lexedData => {
     // Assign Paragraph Variable
     let endParagraph = false;
     let paragraphValue = [];
-    // First, divide lexed Data by paragraph
-    lexedData.forEach((data, index) => {
+    // First, split lexed Data by paragraph
+    for(let index = 0; index< lexedData.length; index++){
+        let data = lexedData[index];
         if(data.value === "" && endParagraph) endParagraph = false;
         else if(data.value === "" && !endParagraph) endParagraph = true;
-        else paragraphValue.push(data)
+        else{
+            let newData = {};
+            // Checking the type of each data
+            if(data.includes.heading){
+                newData.type = "heading";
+                newData.headingLevel = 0;
+                for(let j = 0; j< data.value.length; j++){
+                    if(data.value[j] !== "#") break
+                    else newData.headingLevel += 1
+                }
+                newData.value = data.value.substring(newData.headingLevel + 1)
+                paragraphValue.push(newData)
+            }
+            // Temporary data (will be changed later)
+            else paragraphValue.push(data)
+        }
         // If it's the end of paragraph or it's end of the file
         if(endParagraph || data.lastElement){
             //Reset Variable to Default
@@ -16,8 +32,25 @@ const Parse = lexedData => {
             parsedData.push(paragraphValue)
             paragraphValue = []
         }
-    })
-    return parsedData;
+    }
+    // Convert parsed data to HTML tags
+    const toHTML = (data) => {
+        let htmlData = ""
+        for(let i = 0; i< data.length; i++){
+            if(data[i].type == "heading"){
+                htmlData += `<h${data[i].headingLevel}>${data[i].value}</h${data[i].headingLevel}>`
+            }else{
+                htmlData += "<br />"
+            }
+        }
+        return htmlData
+    }
+    //Gather all parsed information info html tags
+    let parsedHtml = "";
+    for(let i = 0; i< parsedData.length; i++){
+        parsedHtml += `<p>${toHTML(parsedData[i])}</p>`
+    }
+    return parsedHtml;
 }
 
 module.exports = { Parse }
