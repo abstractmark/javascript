d7f3d42811223bfe32e7d92c8a55ac5868704dc8
kimlimjustin <kimlimjustin@gmail.com>
2021-01-20 15:02:12 +0700

    Reorganized CLI options

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {Tokenize} = require('./src/tokenizer');
 const {Lex} = require('./src/lexer');
 const { Parse } = require('./src/parser');
+const {exec} = require('child_process');
 const version = require('./package.json').version;
 // Code for command line command
 
@@ -9,7 +10,8 @@ Usage:
  » For Converting Abstractmark : run "abstractmark [abstractmark file] [abstractmark options] [args]"
  » For Abstractmark's Information : run "abstractmark [option]"
 Example:
- » (Convert Markdown to HTML) abstractmark example.am -o example.html
+ » (Convert Markdown to HTML) abstractmark example.am
+ » (Convert Markdown to certain HTML file name) abstractmark example.am myfile.html
  » (Checking the Current Version of Abstractmark) abstractmark -v
 
 Abstractmark information options:
@@ -17,8 +19,14 @@ Abstractmark information options:
  --help .................. informations about AbstractMark CLI
 
 Abstractmark converting options:
- -o, -output ............. Convert to a specific file name, syntax: "abstractmark [abstractmark source] -o [output file name]"
+ -open ................... Open html file in browser after finish converting.
 `
+// Clear last line output on CLI.
+const CLEAR_LAST_LINE = () => {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+}
+
 const cli = () => {
   const args = process.argv.slice(2)
     if(args.length > 0){
@@ -42,13 +50,29 @@ const cli = () => {
         const tokenizedData = Tokenize(data)
         const lexedData = Lex(tokenizedData)
         const parsedData = Parse(lexedData)
-        console.log(parsedData)
-        if(args[1] === "-o" || args[1] === "-output" && args[2]){
-          fs.writeFile(args[2], parsedData, (err) => {
-            if(err) throw new Error(err)
-            else console.log(`Sucess converted ${file} to ${args[2]}`)
-          })
+        // Write convert result into html file
+        let htmlFileName = null;
+        for(let i = 1; i< args.length; i++){
+          if(!args[i].startsWith('-')) htmlFileName = args[i];
         }
+        process.stdout.write('Converting...')
+        // If user doesn't provide file name
+        if(!htmlFileName) htmlFileName = `${file.split('.').shift()}.html`
+        fs.writeFile(htmlFileName, parsedData, (err) => {
+          if(err) throw new Error(err)
+          else{
+            CLEAR_LAST_LINE()
+            process.stdout.write(`\nSuccessfully converted ${file} to ${htmlFileName}\n`)
+            for(let i = 1; i< args.length; i++){
+              if(args[i] === "-open"){
+                process.stdout.write(`Opening ${htmlFileName} on your browser.`)
+                exec(htmlFileName);
+                CLEAR_LAST_LINE()
+                process.stdout.write(`Opened ${htmlFileName} on your browser.\n`)
+              }
+            }
+          }
+        })
       }
     }else{
       console.log("Usage: abstractmark [abstractmark file] [abstractmark options] [args]\n\nSee \"abstractmark --help\" for more.")
